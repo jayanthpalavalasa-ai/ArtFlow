6f9fbce5fba7d4009bc90cf289efdf3e6b5afc79
jayanth <jayanthpalavalasa@gmail.com>
2026-08-08 14:52:45 +0530

    Connect express server to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("ArtFlow API is running!");
+app.get('/', (req, res) => {
+  res.send('ArtFlow API is running');
 });
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection failed:', err.message);
+  });
